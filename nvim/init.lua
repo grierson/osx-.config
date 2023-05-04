c9121bc6e10257e69bb9611ee1239e26182261fb
Kyle Grierson <griersonky@gmail.com>
2023-05-04 17:10:40 +0100

    fix lsp keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -143,6 +143,14 @@ require("neodev").setup()        -- Plugin dev
 -- LSP + Complete
 local lsp = require('lsp-zero').preset({})
 
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({
+		buffer = bufnr,
+		preserve_mappings = false
+	})
+	vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = true })
+end)
+
 lsp.ensure_installed({
 	"lua_ls",
 	"clojure_lsp",
@@ -153,14 +161,7 @@ lsp.ensure_installed({
 	"marksman",
 })
 
-lsp.on_attach(function(_, bufnr)
-	local opts = { buffer = bufnr, remap = false }
-
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-end)
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
 
