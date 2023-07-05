b874bac66f8d580841842df7d62c423ff8d5a9f6
Kyle Grierson <griersonky@gmail.com>
2023-07-05 10:18:01 +0100

    neorg

--- nvim/init.lua
+++ nvim/init.lua
@@ -51,6 +51,30 @@ require("lazy").setup({
 		build = ":call mkdp#util#install()",
 	},
 
+	-- Neorg
+	{
+		"nvim-neorg/neorg",
+		build = ":Neorg sync-parsers",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		run = ":Neorg sync-parsers", -- This is the important bit!
+		config = function()
+			require("neorg").setup {
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = "~/notes",
+							},
+						},
+					},
+				},
+			}
+		end,
+	},
+
 	-- Search
 	"nvim-telescope/telescope.nvim",
 	{
